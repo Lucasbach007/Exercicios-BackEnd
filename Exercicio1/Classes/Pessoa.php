bf1d2a66c6aec39c81dd1531be3bdaf508df2806
Lucas Bach <140021593+Lucasbach007@users.noreply.github.com>
2025-09-12 20:09:34 -0300

    Update Pessoa.php

--- Exercicio1/Classes/Pessoa.php
+++ Exercicio1/Classes/Pessoa.php
@@ -9,6 +9,13 @@ function _cosntruct($nome, $idade, $peso, $altura){
     $this->peso=$peso;
    
 }
-$ps1 = new Pessoa ();
+    public function exibirDados() {
+        echo "Nome: {$this->nome}<br>";
+        echo "Idade: {$this->idade} anos<br>";
+        echo "Peso: {$this->peso} kg<br>";
+        echo "Altura: {$this->altura} m<br>";
+    }
 
+$ps1 = new Pessoa("Lucas", 22, 70, 1.75);
+$ps1->exibirDados();
 }
