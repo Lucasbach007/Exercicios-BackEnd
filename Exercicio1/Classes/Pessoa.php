e2c64c8369e322531bf6e31e8fac39130bec71e4
Lucas Bach <140021593+Lucasbach007@users.noreply.github.com>
2025-09-11 21:40:16 -0300

    ClassePessoa

--- Exercicio1/Classes/Pessoa.php
+++ Exercicio1/Classes/Pessoa.php
@@ -1,3 +1,12 @@
-public Pessoa{$nome, $idade, $peso, $altura;
-    Private IMC;
+<?php
+Class Pessoa{
+public $nome, $idade, $peso, $altura;
+   
+function _cosntruct($nome, $idade, $peso, $altura){
+    $this->nome=$nome;
+    $this->idade=$idade;
+    $this->peso=$altura;
+    $this->peso=$peso;
+   
+}
 }
