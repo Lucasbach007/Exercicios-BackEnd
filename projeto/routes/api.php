5525cabed1622385ef58265be5ec324caca06f97
Lucas Bach <lucasbach013@gmail.com>
2025-12-13 14:08:59 -0300

    auteracoes

--- projeto/routes/api.php
+++ projeto/routes/api.php
@@ -7,6 +7,10 @@ use App\Models\Servicos;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\UsuarioController;
 
+Route::apiResource('/servicos', ServicosController::class);
+Route::apiResource('/produtos', ProdutoController::class);
+Route::apiResource('/usuarios', UsuarioController::class);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -7,6 +7,28 @@ use App\Http\Controllers\UsuarioController;
 //rota produtos
 Route::get('produtos',[ProdutoController::class, 'index']
 );
+Route::prefix('servicos')->group(function () {
+    Route::get('/', [App\Http\Controllers\ServicosController::class, 'index'])->name('servicos.index');
+    Route::get('/create', [App\Http\Controllers\ServicosController::class, 'create'])->name('servicos.create');
+    Route::post('/', [App\Http\Controllers\ServicosController::class, 'store'])->name('servicos.store');
+    Route::get('/{servicos}', [App\Http\Controllers\ServicosController::class, 'show'])->name('servicos.show');
+    Route::get('/{servicos}/edit', [App\Http\Controllers\ServicosController::class, 'edit'])->name('servicos.edit');
+    Route::put('/{servicos}', [App\Http\Controllers\ServicosController::class, 'update'])->name('servicos.update');
+    Route::delete('/{servicos}', [App\Http\Controllers\ServicosController::class, 'destroy'])->name('servicos.destroy');
+});
+
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [App\Http\Controllers\ProdutoController::class, 'index'])->name('produtos.index');
+    Route::get('/create', [App\Http\Controllers\ProdutoController::class, 'create'])->name('produtos.create');
+    Route::post('/', [App\Http\Controllers\ProdutoController::class, 'store'])->name('produtos.store');
+    Route::get('/{produtos}', [App\Http\Controllers\ProdutoController::class, 'show'])->name('produtos.show');
+    Route::get('/{produtos}/edit', [App\Http\Controllers\ProdutoController::class, 'edit'])->name('produtos.edit');
+    Route::put('/{produtos}', [App\Http\Controllers\ProdutoController::class, 'update'])->name('produtos.update');
+    Route::delete('/{produtos}', [App\Http\Controllers\ProdutoController::class, 'destroy'])->name('produtos.destroy');
+});
+
+
+
 //rota usuarios
 Route::get('usuarios',[UsuarioController::class, 'index']);
 
@@ -14,11 +36,7 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
